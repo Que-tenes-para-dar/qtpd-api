7f70eae2c3efe575df7618596c30552fa98ccde1
Alvaro Farina <farinalvaro@gmail.com>
2020-01-17 11:53:41 -0300

    fixed login error

--- src/routers/loginRoutes.js
+++ src/routers/loginRoutes.js
@@ -7,7 +7,7 @@ const loginRoutes = new express.Router();
 // =========================================================================
 // User login, returns user data and jwt
 // =========================================================================
-loginRoutes.post('/', (req, res) => {
+loginRoutes.post('/', (req, res, next) => {
     return userService.login(req.body.email, req.body.password).then(userAndToken => res.status(200).header('x-auth', userAndToken.token).send({
         success: true,
         data: userAndToken.user
